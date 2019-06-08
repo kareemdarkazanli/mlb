473c6eaee53280d9b78c7128d714a86a78819ea2
Kareem Darkazanli <kdark93@gmail.com>
2019-06-08 16:02:53 -0700

    implemented js logic

---
+++ App.js
@@ -0,0 +1,141 @@
+/*eslint-env browser*/
+
+const helpers = new Helpers();
+const apis = new APIs();
+const MAX_CELLS_DISPLAYED = 5;
+
+
+
+async function init() {
+    const mlbStats = await apis.getMLBStats(); 
+    const overviewData = helpers.parseDataForOverview(mlbStats)
+    const userInterface = new UserInterface();
+    const remoteController = new RemoteController(userInterface, overviewData);
+}
+
+function UserInterface() {
+    
+    const root = document.getElementById('root');
+    root.focus();
+    
+    const overviewCell = (item) => {
+        
+        const {headline, blurb, image} = item;
+        
+        return (
+        `<li class='overview-cell'>
+            <p>${headline}</p>
+        </li>`
+        )
+    }
+    
+    const setOverviewListView = (data, start, end) => {
+        
+        let listItems = "";
+        for(let i = start; i < end; i++) {
+            listItems += overviewCell(data[i]);
+        }
+        root.innerHTML = `<ul id='overview-listview'>${listItems}</ul>`
+        
+    }
+    
+    const getOverviewListView = () => {
+        return document.getElementById('overview-listview');
+    }
+    
+    
+    return {
+        setOverviewListView,
+        getOverviewListView,
+    }
+    
+}
+
+function RemoteController(userInterface, data) {
+    const state = {
+        currentIndex: 0
+    };
+
+    userInterface.setOverviewListView(data, 0, MAX_CELLS_DISPLAYED);
+    userInterface.getOverviewListView().childNodes[state.currentIndex % MAX_CELLS_DISPLAYED].classList.add('highlighted');
+    
+    document.getElementById('root').addEventListener('keydown', (event) => {
+        const {keyCode} = event;
+        
+        if(keyCode === 37 || keyCode === 39) {
+            let {currentIndex} = state;
+
+            userInterface.getOverviewListView().childNodes[currentIndex % MAX_CELLS_DISPLAYED].classList.remove('highlighted');
+            if(keyCode === 37) {
+                currentIndex = currentIndex > 0 ? currentIndex - 1 : currentIndex;
+                if(currentIndex >= 0) {
+                    scrollLeft(userInterface, currentIndex);
+                }
+            }
+            else {
+                currentIndex = currentIndex < data.length - 1 ? currentIndex + 1 : currentIndex;
+                if(currentIndex <= data.length - 1) {
+                    scrollRight(userInterface, currentIndex);
+                }
+            }
+            state.currentIndex = currentIndex;
+        }
+        
+        
+    }, true);
+    
+    const scrollLeft = (userInterface, currentIndex) => {
+        if((currentIndex + 1) % MAX_CELLS_DISPLAYED === 0 && currentIndex > 0) {
+            userInterface.setOverviewListView(data, currentIndex + 1 - MAX_CELLS_DISPLAYED, currentIndex + 1);
+        }
+        userInterface.getOverviewListView().childNodes[currentIndex % MAX_CELLS_DISPLAYED].classList.add('highlighted');
+    }
+    
+    const scrollRight = (userInterface, currentIndex) => {
+        if(currentIndex % MAX_CELLS_DISPLAYED === 0 && currentIndex < data.length - 1) {
+            userInterface.setOverviewListView(data, currentIndex, currentIndex + MAX_CELLS_DISPLAYED);
+        }
+        userInterface.getOverviewListView().childNodes[currentIndex % MAX_CELLS_DISPLAYED].classList.add('highlighted');
+    }
+}
+
+function APIs() {
+    async function getMLBStats() {
+        const response = await fetch('http://statsapi.mlb.com/api/v1/schedule?hydrate=game(content(editorial(recap))),decisions&date=2018-06-10&sportId=1');
+        const stats = await response.json();
+        return stats;
+    }   
+    return {
+        getMLBStats
+    }
+}
+
+function Helpers() {
+    const parseDataForOverview = (data) => {
+        console.log(data)
+        const {dates=[]} = data;
+        const {games=[]} = dates[0];
+        
+        const overview = games.reduce((acc, game) => {
+            const {content={}} = game;
+            const {editorial={}} = content;
+            const {recap={}} = editorial;
+            const {mlb={}} = recap;
+            const {headline="", blurb="", image={}} = mlb;
+            acc.push({
+                headline,
+                blurb,
+                image
+            });
+            return acc;
+        }, []);
+        
+        return overview;
+    }
+    
+    return {
+        parseDataForOverview
+    }
+}
+
+init();
